c5d71919c8368cea9f4b39117ebeb34a853379fa
tamellaravi <ravitamella789@gmail.com>
2020-07-26 12:18:59 +0530

    ansiblemaster

--- ansible/ansiblenode.sh
+++ ansible/ansiblenode.sh
@@ -2,7 +2,8 @@
 sudo apt update 
 sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible -y 
+sudo apt-get install python -y
+
 
 sudo adduser ansible << ENDX
 ansible
@@ -14,10 +15,15 @@ First Last
 
 Y
 ENDX
-
 sudo sh -c "echo \"ansible ALL=(ALL:ALL) NOPASSWD: ALL\" >> /etc/sudoers"
 
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 sudo service sshd restart
 sudo su ansible
-ansible 
+ansible
+
+
+
+
+
+
