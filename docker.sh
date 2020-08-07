9661b63778702b54b432905f1c1c89ad62cfa1ac
tamellaravi <ravitamella789@gmail.com>
2020-08-07 10:01:01 +0530

    docker

---
+++ docker.sh
@@ -0,0 +1,21 @@
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo usermod -aG docker ubuntu
+sudo chmod 666 /var/run/docker.sock
+echo 
+ubuntu id added to docker group
+echo
+sudo systemctl status docker
+
