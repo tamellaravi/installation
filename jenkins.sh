b874da849c75e7da9128b38e0619eb968783956f
tamellaravi <ravitamella789@gmail.com>
2020-08-07 09:46:08 +0530

    url

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,8 @@
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \ /etc/apt/sources.list.d/jenkins.list'
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install jenkins -y
 sudo systemctl start jenkins 
